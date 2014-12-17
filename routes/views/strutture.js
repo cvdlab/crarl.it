47807d8d66732b26fa20372880c0e584e773093c
Federico Spini <federico.spini@gmail.com>
2014-12-17 13:04:28 +0100

    fixed structures controller

--- routes/views/strutture.js
+++ routes/views/strutture.js
@@ -7,9 +7,9 @@ exports = module.exports = function(req, res) {
     locals = res.locals;
   
   // Init locals
-  locals.section = 'strutture';
+  locals.section = 'structures';
   locals.data = {
-    strutture: []
+    structures: []
   };
   
   // Load all the structures
