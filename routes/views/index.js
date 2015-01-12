e2b33521c9e9cc8ecc1cacff960770f90b62efce
Federico Spini <federico.spini@gmail.com>
2015-01-12 16:37:36 +0100

    added "ultime notizie" and "Documenti recenti" sections in homepage

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,15 +1,51 @@
 var keystone = require('keystone');
+var last_news = 5;
+var last_docs = 6;
 
 exports = module.exports = function(req, res) {
-	
-	var view = new keystone.View(req, res),
-		locals = res.locals;
-	
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'home';
-	
-	// Render the view
-	view.render('index');
-	
+  
+  var view = new keystone.View(req, res);
+  var locals = res.locals;
+  
+  // Init locals
+  locals.data = {
+    news: [],
+    documents: []
+  };
+
+  // locals.section is used to set the currently selected
+  // item in the header navigation.
+  locals.section = 'home';
+
+  // Load last news
+  view.on('init', function(next) {
+    var q = keystone.list('News').model
+      .where('state', 'published')
+      .sort('-publishedDate')
+      .limit(last_news)
+      .populate('author categories');
+      
+      q.exec(function(err, results) {
+        locals.data.news = results;
+        next(err);
+      });
+  });
+  
+  // Load recent docs
+  view.on('init', function(next) {
+    var q = keystone.list('Document').model
+      .where('state', 'published')
+      .sort('-publishedDate')
+      .limit(last_docs)
+      .populate('author categories');
+      
+      q.exec(function(err, results) {
+        locals.data.documents = results;
+        next(err);
+      });
+  });
+
+  // Render the view
+  view.render('index');
+  
 };
